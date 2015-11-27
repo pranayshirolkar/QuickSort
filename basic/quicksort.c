49b0df56d5bd8fa6a5ab4403b439bea606bddbf4
Pranay Shirolkar <pranayshirolkar@gmail.com>
2015-11-27 14:51:47 -0600

    Array parsed from input text file

--- basic/quicksort.c
+++ basic/quicksort.c
@@ -1,17 +1,27 @@
-#include<stdio.h>
-int main()
+#include<stdio.h> 
+#include<stdlib.h>
+int main(int argc, char *argv[]) 
 {
-
-A
-A
-B
-B
-Bi
-B
-B
-C
-D
-C
-inntT
-
+	FILE *inputFile;
+	int arraySize,i;
+	double *array;
+	if(argc!=2)
+	{
+		printf("usage: %s <filename>\n",argv[0]);
+		return 0;
+	}
+	//building array out of the input file
+	inputFile=fopen(argv[1],"r");
+	if (inputFile == NULL) return 0;
+	fscanf(inputFile,"%d",&arraySize);
+	printf("arraySize: %d\n",arraySize);
+	i=0;
+	array=(double *)malloc(sizeof(double)*(arraySize));
+	while(i<arraySize)
+	{
+	fscanf(inputFile,"%lf",&array[i]);
+	printf("array[%d]: %.2f\n",i,array[i]);
+	i++;
+	}
+	return 0;
 }
