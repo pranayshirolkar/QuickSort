00e48dc325ef041e02c038e46e2731762931cf93
Pranay Shirolkar <pranayshirolkar@gmail.com>
2015-11-27 22:41:30 -0600

    Basic (Classical) Quicksort implementation complete

--- basic/quicksort.c
+++ basic/quicksort.c
@@ -1,5 +1,7 @@
 #include<stdio.h> 
 #include<stdlib.h>
+void quickSort(double [], int, int);
+int partition(double [], int, int);
 int main(int argc, char *argv[]) 
 {
 	FILE *inputFile;
@@ -23,5 +25,73 @@ int main(int argc, char *argv[])
 	printf("array[%d]: %.2f\n",i,array[i]);
 	i++;
 	}
+	printf("Array building from file complete...\n");
+	quickSort(array,0,arraySize-1);
+	printf("Sorting Complete\n");
+	i=0;
+	while(i<arraySize)
+{
+	printf("%.2f\n",array[i]);
+i++;
+}
 	return 0;
+}
+void quickSort(double array[], int left, int right)
+{
+//double pivot;
+//pivot=array[left];
+int partitionPoint=partition(array,left,right);
+if(!(left==partitionPoint-1))
+quickSort(array,left,partitionPoint-1);
+if(!(partitionPoint==right))
+quickSort(array,partitionPoint,right);
+}
+int partition(double array[], int left, int right)
+{
+	double pivot=array[left];
+	double tmp;
+	int a;
+	int i,j;
+	i=left;
+	j=right;
+	printf("left: %d right: %d\n",i,j);
+	printf("Pivot: %.2f\n",pivot);
+	scanf("%d",&a);
+	while(i<=j)
+	{
+		while(i<=right)
+		{
+			printf("left: at array[%d]:%.2f\n",i,array[i]);
+scanf("%d",&a);
+			if(array[i]>=pivot)
+			{
+				break;
+			}
+			i++;
+		}
+		while(j>=left)
+		{
+			printf("right: at array[%d]:%.2f\n",j,array[j]);
+scanf("%d",&a);
+			if(array[j]<=pivot)
+			{
+				break;
+			}
+			j--;
+		}
+		if(i<=j)
+		{
+			//swap
+			printf("Swapping values [%d]: %.2f and [%d]: %.2f now...\n",i,array[i],j,array[j]);
+scanf("%d",&a);
+			tmp=array[i];
+			array[i]=array[j];
+			array[j]=tmp;
+			i++;
+			j--;
+		}
+	}
+printf("returning: %d",i);
+scanf("%d",&a);
+	return i;
 }
